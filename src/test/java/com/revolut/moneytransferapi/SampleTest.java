e358ecc9a63c649604135239dd390439ac53722e
Eyup Aksu <eyup.aksu@worldfirst.com>
2019-09-24 13:48:58 +0100

    Initial commit

---
+++ src/test/java/com/revolut/moneytransferapi/SampleTest.java
@@ -0,0 +1,13 @@
+package com.revolut.moneytransferapi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+  @Test
+  public void sampleTest(){
+    Assert.assertTrue(true);
+  }
+
+}
